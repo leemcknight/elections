0553abb9a73f8130707571814499735956a1cdb4
Lee McKnight <lee_mcknight@hotmail.com>
2020-12-29 20:06:45 -0600

    move to a push-down of the state

--- web/dashboard/src/App.js
+++ web/dashboard/src/App.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import React, { useState } from 'react';
+import React, { useEffect, useState} from 'react';
 import NationwideTotals from './components/NationwideTotals';
 import StateModal from './pages/StateModal';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,15 +16,95 @@ function App() {
     'trump': '#ff0000'
   };
 
+  const baseUrl = "https://ip6rswdf3m.execute-api.us-west-2.amazonaws.com/dev";
+  const headers = { Accept: "application/json" };        
   const geoUrl = "/geodata/us-albers.json"    
   const [nationalData, setNationalData] = useState({states: {}});
   const [selectedState, setSelectedState] = useState();
-  const [registrationData, setRegistrationData] = useState();
-  const [stateData, setStateData] = useState();
+  const [stateTotals, setStateTotals] = useState({});
+  const [selectedStateVotes, setSelectedStateVotes] = useState();
+  const [selecteStateRegistrations, setSelectedStateRegistrations] = useState();
+  const [error, setError] = useState();
+  const [busy, setBusy] = useState({
+    registrationData: false,
+    voteData: false
+  });
+
+  useEffect(() => {
+    setBusy({
+        registrationData: true,
+        voteData: true
+    });
+    console.log(`getting votes for ${selectedState}`);
+    fetch(`${baseUrl}/votes/state/${selectedState}`, {headers})
+        .then(result => result.json())
+        .then(result => { addVoteData(result); })
+        .catch(error => setError(error))
+        .finally(setBusy({
+          registrationData: busy.registrationData,
+          voteData: false
+        }));
+    
+    fetch(`${baseUrl}/registrations/state/${selectedState}`, {headers})
+        .then(result => result.json())
+        .then(result => { addRegistrationData(result);})
+        .catch(error => setError(error))
+        .finally(setBusy({
+          registrationData: false,
+          voteData: busy.voteData
+        }));
+  }, [selectedState]);
+
+  const addRegistrationData = (registrationData) => {
+      let newStateTotals = _.clone(stateTotals);      
+      console.log(`newStateTotals in registration data: ${newStateTotals}`)
+      if(!newStateTotals[selectedState]) {
+        console.log(`initializeing newStateTotals for ${selectedState}`);
+        newStateTotals[selectedState] = {};
+      }
+
+      newStateTotals[selectedState].registrationData = registrationData;
+      setStateTotals(newStateTotals);
+
+      setSelectedStateRegistrations(registrationData);      
+  }
+
+  const addVoteData = (voteData) => {
+      let newStateTotals = _.clone(stateTotals);      
+      console.log(`newStateTotals in vote data: ${newStateTotals}`);
+      if(!newStateTotals[selectedState]) {
+        console.log(`initializeing newStateTotals for ${selectedState}`);
+        newStateTotals[selectedState] = {};
+      }
+
+      newStateTotals[selectedState].voteData = voteData;
+      setStateTotals(newStateTotals);
+
+      //update national totals
+      let stateData = {}; 
+      let candidates = [];    
+      const counties = voteData.counties;    
+      for(const county in counties) {
+        for(const candidateVotes in counties[county].votes) {            
+              candidates.push(candidateVotes);
+              const numVotes = parseInt(counties[county].votes[candidateVotes]);            
+              if(!stateData[candidateVotes]) {            
+                stateData[candidateVotes] = numVotes;
+              } else {            
+                stateData[candidateVotes] += numVotes;
+              }            
+          }
+      }      
+      const newNationalData = _.clone(nationalData);    
+      newNationalData.states[selectedState] = stateData;    
+      setNationalData(newNationalData);
+      
+      setSelectedStateVotes(voteData);      
+  }
 
   const onGeographyClick = geography => event => {
-    const state= geography.properties.iso_3166_2;
-    setSelectedState(state);    
+    const state= geography.properties.iso_3166_2;    
+    setSelectedState(state);
   }
 
   const handleModalClose = () => {
@@ -54,37 +134,6 @@ function App() {
       color = stateColor[winner];    
     }
     return color;
-  }
-
-  const stateReportingCallback = (state, voteData) => {    
-    if(!voteData) {
-      return;
-    }    
-    
-    let stateData = {}; 
-    let candidates = [];    
-    const counties = voteData.counties;    
-    for(const county in counties) {        
-      for(const candidateVotes in counties[county].votes) {            
-            candidates.push(candidateVotes);
-            const numVotes = parseInt(counties[county].votes[candidateVotes]);            
-            if(!stateData[candidateVotes]) {            
-              stateData[candidateVotes] = numVotes;
-            } else {            
-              stateData[candidateVotes] += numVotes;
-            }            
-        }
-    }
-    
-    const newNationalData = _.clone(nationalData);    
-    newNationalData.states[state] = stateData;    
-    setNationalData(newNationalData);
-  }
-
-  const registrationReportingCallback = (state, registrationData) => {
-      const newRegistrationData = _.clone(registrationData);
-      registrationData.states[state] = registrationData;
-      setRegistrationData(newRegistrationData);
   }
   
   return (
@@ -120,8 +169,8 @@ function App() {
     <StateModal showModal={selectedState != null} 
                 state={selectedState} 
                 onHide={handleModalClose} 
-                reportingCallback={stateReportingCallback}
-                registrationReportingCallback={registrationReportingCallback} />       
+                stateRegistrations={selecteStateRegistrations}
+                stateVotes={selectedStateVotes} />       
     </>
   );
 }
--- web/dashboard/src/pages/StateModal.js
+++ web/dashboard/src/pages/StateModal.js
@@ -5,66 +5,22 @@ import Row from 'react-bootstrap/esm/Row';
 import StateGague from '../components/StateGague';
 import Col from 'react-bootstrap/Col';
 import Modal from 'react-bootstrap/Modal';
-import Spinner from 'react-bootstrap/Spinner';
 import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CountyResults from '../components/CountyResults';
 
-function StateModal(props) {
-    
-    const baseUrl = "https://ip6rswdf3m.execute-api.us-west-2.amazonaws.com/dev";
+function StateModal(props) {    
     const state = props.state;
-    const headers = { Accept: "application/json" };        
-    const [data, setData] = useState();
-    const [registrationData, setRegistrationData] = useState();
-    const [error, setError] = useState();
+    const stateVotes = props.stateVotes;
+    const stateRegistrations = props.stateRegistrations;
     const [selectedCounty, setSelectedCounty] = useState();
-    const [busy, setBusy] = useState({
-        registrationData: false,
-        voteData: false
-    });
     const url = "/geodata/" + state + ".json"
-    let stateTotals;
 
-    function calcStateTotals(results) {
-
-    }
-
-    function calcRegistrationTotals(results) { 
-        
-    }
-
-    function isBusy() {
-        return (busy.registrationData || busy.voteData);
-    }
-
-    useEffect(() => {
-        setBusy({
-            registrationData: true,
-            voteData: true
-        });
-        console.log(`getting votes for ${state}`);
-        fetch(`${baseUrl}/votes/state/${state}`, {headers})
-            .then(result => result.json())
-            .then(result => { setData(result);
-                             calcStateTotals(result);
-                             props.reportingCallback(state, result);})
-            .catch(error => setError(error))
-            .finally(setBusy(false));
-        
-        fetch(`${baseUrl}/registrations/state/${state}`, {headers})
-            .then(result => result.json())
-            .then(result => { setRegistrationData(result);
-                             calcRegistrationTotals(result);
-                             props.registrationReportingCallback(state, result);})
-            .catch(error => setError(error))
-            .finally(setBusy(false));
-    }, [state]);
-    
     function getCountyColor(geography) {                       
         const countyName = geography.properties.NAME.toUpperCase();
-        if(data) {                               
-            const county = data.counties[countyName];            
+        
+        if(stateVotes) {                               
+            const county = stateVotes.counties[countyName];            
             if(!county) {            
                 return "#454545";
             }            
@@ -84,12 +40,12 @@ function StateModal(props) {
     const onGeographyClick = geography => event => {        
         const clickedCounty = geography.properties.NAME.toUpperCase();
         let votes, countyRegistration;
-        if(data) {
-            votes = data.counties[clickedCounty];
+        if(stateVotes) {
+            votes = stateVotes.counties[clickedCounty];
         }
 
-        if(registrationData) {            
-            const countyRegistrations = registrationData.filter(county => county.county === clickedCounty);            
+        if(stateRegistrations) {
+            const countyRegistrations = stateRegistrations.filter(county => county.county === clickedCounty);            
             if(countyRegistrations && countyRegistrations.length > 0) {
                 countyRegistration = countyRegistrations[0];
             }
@@ -100,78 +56,55 @@ function StateModal(props) {
             registrationData: countyRegistration
         });
     }
-    
-
-    if(data && !isBusy()) {        
-        return (            
-            <Modal show={props.showModal} 
-                    onHide={props.onHide}                                         
-                    >
-                <Modal.Header closeButton>
-                    <h3>{props.state}</h3>
-                </Modal.Header>
-                <Modal.Body>                           
-                    <Container>
-                        <Row>
-                            <Col>
-                                <ComposableMap projection="geoAlbers">
-                                    <ZoomableGroup>
-                                    <Geographies geography={url}>
-                                        {({ geographies }) =>
-                                        geographies.map(geo => {                                     
-                                            return (
-                                            <Geography
-                                                key={geo.rsmKey}
-                                                geography={geo}  
-                                                fill={getCountyColor(geo)}
-                                                onClick={onGeographyClick(geo)}
-                                            />
-                                            );
-                                        })
-                                        }
-                                    </Geographies>
-                                    </ZoomableGroup>
-                                </ComposableMap>
-                            </Col>
-                        </Row>                        
-                        <Row>
-                            <Col>
-                                <CountyResults county={selectedCounty} />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <StateGague></StateGague>
-                            </Col>
-                        </Row>
-                    </Container>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={props.onHide}>Close</Button>
-                </Modal.Footer>
-            </Modal>
-        );
-    } else if(error) {        
-        return (
-            <Modal show={props.showModal} size="lg">
-                <Modal.Header>Error</Modal.Header>
-                <Modal.Body>{error.message}</Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={props.onHide}>Close</Button>
-                </Modal.Footer>
-            </Modal>
-        )
-    } else {    
-        return (
-            <Modal show={props.showModal}>
-                <Modal.Header>Loading</Modal.Header>
-                <Modal.Body>                    
-                    <Spinner>                             
-                    </Spinner>                        
-                </Modal.Body>                
-            </Modal>
-        );
-    }
+        
+    return (            
+        <Modal show={props.showModal} 
+                onHide={props.onHide}                    
+                >
+            <Modal.Header closeButton>
+                <h3>{props.state}</h3>
+            </Modal.Header>
+            <Modal.Body>                           
+                <Container>
+                    <Row>
+                        <Col>
+                            <ComposableMap projection="geoAlbers">
+                                <ZoomableGroup>
+                                <Geographies geography={url}>
+                                    {({ geographies }) =>
+                                    geographies.map(geo => {                                     
+                                        return (
+                                        <Geography
+                                            key={geo.rsmKey}
+                                            geography={geo}  
+                                            fill={getCountyColor(geo)}
+                                            onClick={onGeographyClick(geo)}
+                                        />
+                                        );
+                                    })
+                                    }
+                                </Geographies>
+                                </ZoomableGroup>
+                            </ComposableMap>
+                        </Col>
+                    </Row>                        
+                    <Row>
+                        <Col>
+                            <CountyResults county={selectedCounty} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <StateGague></StateGague>
+                        </Col>
+                    </Row>
+                </Container>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
 }
 
 export default StateModal;
